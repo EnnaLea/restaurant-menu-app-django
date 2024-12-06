92bc8384dcc4063908709aa41c2bee2cb458b96f
EnnaLea <soleariaeve@gmail.com>
2024-12-06 11:24:02 +0100

    context

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,3 +1,5 @@
+from lib2to3.fixes.fix_input import context
+
 from django.shortcuts import render
 from django.views import generic
 from .models import Item
@@ -6,6 +8,12 @@ from .models import Item
 class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context_data = {"meals": ["Pizza", "Pasta"],
+                        "ingredients": ["things"]}
+        return context_data
+
 
 
 class MenuItemDetail(generic.DetailView):
